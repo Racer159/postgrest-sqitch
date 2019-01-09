2b065a61d025ef3a1f1a69b1820ef1fae3a2d56b
Wayne Starr <starr@diux.mil>
2019-01-09 10:51:28 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## PostgREST - Sqitch Docker Image
+
+A docker image pulling in both PostgREST and Sqitch together.  `start.sh` and `Dockerfile` courtesy of [theasp/docker-postgrest-sqitch](https://github.com/theasp/docker-postgrest-sqitch/)
---
+++ start.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+# Sourced from https://github.com/theasp/docker-postgrest-sqitch/blob/master/start.sh
+
+set -e
+
+PGRST_DB_URI="${PGRST_DB_URI:-pg://}"
+
+do_sqitch() {
+  sqitch status "db:${PGRST_DB_URI}" || true
+  sqitch ${SQITCH_DEPLOY:-deploy} "db:${PGRST_DB_URI}"
+  if [ "${SQITCH_VERIFY}" ]; then
+    sqitch ${SQITCH_VERIFY} "db:${PGRST_DB_URI}"
+  fi
+}
+
+mkdir -p ~/.sqitch
+cat > ~/.sqitch/sqitch.conf <<EOF
+[core]
+  engine = pg
+EOF
+
+SQITCH_DIR=/sqitch
+SQITCH_PLAN=$SQITCH_DIR/sqitch.plan
+
+if [ -e $SQITCH_PLAN ]; then
+  if [ ${SQITCH_REQUIRED:-auto} = auto ]; then
+    SQITCH_REQUIRED=true
+  fi
+
+  if ! (cd $SQITCH_DIR && do_sqitch); then
+    RC=$?
+
+    if [ ${SQITCH_REQUIRED} = true ]; then
+      echo "ERROR: Sqitch returned $RC" 1>&2
+      exit $RC
+    else
+      echo "WARNING: Sqitch returned $RC" 1>&2
+    fi
+  fi
+else
+  if [ ${SQITCH_REQUIRED} = true ]; then
+    echo "ERROR: Missing $SQITCH_PLAN" 1>&2
+    exit 1
+  else
+    echo "WARNING: Missing $SQITCH_PLAN" 1>&2
+  fi
+fi
+
+export PGRST_QUIET=${PGRST_QUIET:-false}
+if ! grep -q ^quiet /etc/postgrest.conf; then
+  echo 'quiet = "$(PGRST_QUIET)"' >> /etc/postgrest.conf
+fi
+
+exec postgrest /etc/postgrest.conf $PGRST_ARGS
